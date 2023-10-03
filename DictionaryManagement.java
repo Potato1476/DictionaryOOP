8bd3ede6ea96a80d2e6739112beb6fbcc16519d8
Vo Trong Dan <trongdantvh@gmail.com>
2023-10-03 08:34:00 +0700

    add function insertFromCommandline

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DictionaryManagement {
+
+    ArrayList<Word> wordList;
+
+    DictionaryManagement() {
+        wordList = new ArrayList<>();
+    }
+
+    public void insertFromCommandline() {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            String target = input.nextLine();
+            String explain = input.nextLine();
+
+            Word newWord = new Word();
+            newWord.setTaget(target);
+            newWord.setExplain(explain);
+
+            wordList.add(newWord);
+
+        }
+
+        input.close();
+    }
+
 }
